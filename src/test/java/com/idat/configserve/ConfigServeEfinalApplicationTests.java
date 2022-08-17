f01fc4068b073aff1791508e10a953e1082f5fd7
josuepro711 <josue.cisneros@holinsys.net>
2022-08-16 23:48:46 -0500

    configserve

---
+++ src/test/java/com/idat/configserve/ConfigServeEfinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idat.configserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServeEfinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
